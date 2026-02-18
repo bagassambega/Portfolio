6a647242e868121a7a138a41e87b581ad171c8bc
bagassambega <bagassambega.dev@gmail.com>
2026-02-18 11:08:48 +0700

    fix: layout mismatch structure between main and payload

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,34 @@
 import Navbar from "@/components/Navbar"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { Geist, Geist_Mono } from "next/font/google"
 import { ReactNode } from "react"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
-    <>
-      <Navbar />
-      {children}
-    </>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            {children}
+          </ThemeProvider>
+        </div>
+      </body>
+    </html>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next"
 import "./globals.css"
-import { ThemeProvider } from "@/components/ThemeProvider"
-import { Geist, Geist_Mono } from "next/font/google"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,19 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   )
 }
